bffdb04bfd271af337e4182c1f4b508e449f189f
Aistis Raulinaitis <sheganinans@gmail.com>
2018-07-19 14:40:00 -0700

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# rust-assoc-type-talk
+
+Code for Talk @ https://www.meetup.com/oc-rust/events/252639183/
--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ mod step6;
 mod step7;
 
 fn main() {
-    //step1::step1()
+    step1::step1()
     //step2::step2()
     //step3::step3()
     //step4::step4()
     //step5::step5()
     //step6::step6()
-    step7::step7()
+    //step7::step7()
 }
